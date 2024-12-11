ad2c922d6f82e40591db0842284153bfd10bfd6b
Mughees Subtain <mugheessubtain@gmail.com>
2024-12-11 09:14:15 -0800

    first Project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <div className='flex justify-center'>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div> */}
+      <h1 className='m-5'>Welcome to first Project</h1>
+      <h1  className="text-4xl font-sans font-bold">Muhammad Mughees Subtain</h1>
+
+    </>
+  )
+}
+
+export default App
