9295e7c6310d1df9de4af34c6c5aeca6a30a738b
Mughees Subtain <mugheessubtain@gmail.com>
2024-12-12 06:23:08 -0800

    first project

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,9 @@ function App() {
 
   return (
     <>
-      {/* <div className='flex justify-center'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
       <h1 className='m-5'>Welcome to first Project</h1>
       <h1  className="text-4xl font-sans font-bold">Muhammad Mughees Subtain</h1>
+      <h3>Here is my <a href="https://github.com/mugheessubtain/" className='text-blue-500'>Github</a>And <a className='text-blue-500' href="https://www.linkedin.com/in/mughees-subtain-17aab72ba/">Linkedin</a> links</h3>
 
     </>
   )
